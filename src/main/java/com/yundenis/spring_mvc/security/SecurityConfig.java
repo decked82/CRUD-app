cca2eed25c569e63701d54f67f1d357f2b279743
Yun Denis <yundenis1982@gmail.com>
2021-10-04 17:26:03 +0900

    Добавлен Security Config - связано с русской кодировкой

--- src/main/java/com/yundenis/spring_mvc/security/SecurityConfig.java
+++ src/main/java/com/yundenis/spring_mvc/security/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
-@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
--- src/main/java/com/yundenis/spring_mvc/security/SpringSecurityInitializer.java
+++ src/main/java/com/yundenis/spring_mvc/security/SpringSecurityInitializer.java
@@ -3,5 +3,5 @@ package com.yundenis.spring_mvc.security;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-    //пустой класс, использующийся для резистрации модуля в спринг-контейнере
+        //пустой класс, использующийся для резистрации модуля в спринг-контейнере
 }
