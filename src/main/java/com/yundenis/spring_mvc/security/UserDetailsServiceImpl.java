78a05612fee1c964e15f9bcbafccc534b55f48b9
Yun Denis <yundenis1982@gmail.com>
2021-12-15 11:08:35 +0900

    Убрал @Transactional в классе UserDetailsServiceImpl, обновил зависимости в pom файле

--- src/main/java/com/yundenis/spring_mvc/security/UserDetailsServiceImpl.java
+++ src/main/java/com/yundenis/spring_mvc/security/UserDetailsServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -19,7 +18,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     }
 
     @Override
-    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userService.getUsernameByName(username);
         if (user == null) {
